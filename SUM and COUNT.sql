2da09189f99bb4d3fbf4c3270088fd3bb88d93a5
samikshawattamwar <77261236+samikshawattamwar@users.noreply.github.com>
2022-02-09 11:38:55 +0530

    Update SUM and COUNT.sql

--- SUM and COUNT.sql
+++ SUM and COUNT.sql
@@ -1,17 +1,53 @@
---1	World Country Profile: Aggregate functions
+--1	
 --Show the total population of the world
 
 SELECT SUM(population)
 FROM world
 
---2	You might want to look at these examples first
+--2	
+--List all the continents - just once each.
+select distinct continent from world
 
---3	Total world population
---4	List of continents
---5	GDP of Africa
---6	Count the big countries
---7	Baltic states population
---8	Using GROUP BY and HAVING
---9	Counting the countries of each continent
---10	Counting big countries in each continent
---11	Counting big continents
+--3	
+--Give the total GDP of Africa
+
+SELECT SUM(GDP) 
+from world
+WHERE continent = 'Africa'
+
+--4	
+-- How many countries have an area of at least 1000000
+
+select count(name)
+from world
+where area >= 1000000
+
+--5	
+--What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+
+select sum(population) 
+from world
+where name in ('Estonia', 'Latvia', 'Lithuania')
+
+--6	
+--For each continent show the continent and number of countries.
+
+select distinct continent, count(name)
+from world
+group by continent
+
+--7	
+--For each continent show the continent and number of countries with populations of at least 10 million.
+
+select continent, count(name)
+from world
+where population >= 10000000
+group by continent
+
+--8	
+-- List the continents that have a total population of at least 100 million.
+
+select continent 
+from world
+group by continent
+having sum(population) >= 100000000

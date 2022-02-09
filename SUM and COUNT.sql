b03361de019c6af115c3739f73f76263fe500e5c
samikshawattamwar <77261236+samikshawattamwar@users.noreply.github.com>
2022-02-09 10:30:44 +0530

    Create SUM and COUNT.sql

---
+++ SUM and COUNT.sql
@@ -0,0 +1,17 @@
+--1	World Country Profile: Aggregate functions
+--Show the total population of the world
+
+SELECT SUM(population)
+FROM world
+
+--2	You might want to look at these examples first
+
+--3	Total world population
+--4	List of continents
+--5	GDP of Africa
+--6	Count the big countries
+--7	Baltic states population
+--8	Using GROUP BY and HAVING
+--9	Counting the countries of each continent
+--10	Counting big countries in each continent
+--11	Counting big continents

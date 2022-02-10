145afebca58c210c77d3ad0a2643cb5f9d1d1095
samikshawattamwar <77261236+samikshawattamwar@users.noreply.github.com>
2022-02-10 12:13:59 +0530

    Create  The JOIN operation.sql

---
+++ The JOIN operation.sql
@@ -0,0 +1,41 @@
+--1
+--Modify it to show the matchid and player name for all goals scored by Germany. To identify German players
+
+SELECT matchid, player FROM goal 
+  WHERE teamid = 'GER'
+
+--2
+--
+
+--3
+--
+
+--4
+--
+
+--5
+--
+
+--6
+--
+
+--7
+--
+
+--8
+--
+
+--9
+--
+
+--10
+--
+
+--11
+--
+
+--12
+--
+
+--13
+--
